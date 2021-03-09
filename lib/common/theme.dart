3d542d72f29513d69e08297a4d3b96283f4a6107
Martin Jablecnik <martin.jablecnik@email.cz>
2021-03-09 21:29:47 +0100

    Add carousel indicator

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 
 class AppTheme {
+  static const backgroundColor = Color.fromRGBO(5, 6, 31, 1);
+
   static const double avatarWidth = 28.0;
   static const double avatarHeight = 28.0;
 
+  static const int carouselInterval = 5;
+  static const int carouselAnimationDuration = 800;
   static const double carouselHeight = 200.0;
-
-  static const backgroundColor = Color.fromRGBO(5, 6, 31, 1);
+  static const double carouselIndicatorCornerRadius = 0.0;
+  static const double carouselIndicatorSpace = 3.0;
+  static const double carouselIndicatorBottomPosition = AppTheme.carouselHeight - 40;
 }
 
 ThemeData appTheme = ThemeData(
--- lib/pf_app/widgets/carousel.dart
+++ lib/pf_app/widgets/carousel.dart
@@ -1,39 +1,70 @@
+import 'package:carousel_indicator/carousel_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:pixelfield_app/common/constants.dart';
 import 'package:pixelfield_app/common/theme.dart';
 
+class CarouselController extends GetxController {
+  RxInt _currentIndex = 0.obs;
+
+  get currentIndex => _currentIndex.toInt();
+
+  set currentIndex(int index) {
+    _currentIndex.value = index;
+    this.update();
+  }
+}
 
 class Carousel extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: AppTheme.carouselHeight,
-        viewportFraction: 1.0,
-        enableInfiniteScroll: true,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 5),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-      ),
-      items: carouselImages.map((image) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              child: GestureDetector(
-                child: Image.asset(image),
-                onTap: () {
-                  print("Clicked to: ${image.split('/').last}");
+    return GetBuilder<CarouselController>(
+      init: CarouselController(),
+      builder: (CarouselController controller) => Stack(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+                height: AppTheme.carouselHeight,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: AppTheme.carouselInterval),
+                autoPlayAnimationDuration: Duration(milliseconds: AppTheme.carouselAnimationDuration),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                onPageChanged: (int index, _) {
+                  controller.currentIndex = index;
+                }),
+            items: carouselImages.map((image) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: GestureDetector(
+                      child: Image.asset(image),
+                      onTap: () {
+                        print("Clicked to: ${image.split('/').last}");
+                      },
+                    ),
+                  );
                 },
-              ),
-            );
-          },
-        );
-      }).toList(),
+              );
+            }).toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: AppTheme.carouselIndicatorBottomPosition),
+            child: CarouselIndicator(
+              count: carouselImages.length,
+              index: controller.currentIndex,
+              width: MediaQuery.of(context).size.width / carouselImages.length,
+              cornerRadius: AppTheme.carouselIndicatorCornerRadius,
+              space: AppTheme.carouselIndicatorSpace,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
